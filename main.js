9c719907c9a751123d4442fb9b5a0098999e248f
MakerSquare Student <students@themakersquare.com>
2013-11-22 09:59:11 -0600

    Initial commit, went ahead and filled in the rest of the eight possible win cases.

--- main.js
+++ main.js
@@ -28,11 +28,17 @@ var checkForWinner = function () {
   if ( spaces[0] === spaces[1] && spaces[1] === spaces[2]
     || spaces[3] === spaces[4] && spaces[4] === spaces[5]
     || spaces[6] === spaces[7] && spaces[7] === spaces[8]
+    || spaces[0] === spaces[3] && spaces[3] === spaces[6]
+    || spaces[1] === spaces[4] && spaces[4] === spaces[7]
+    || spaces[2] === spaces[5] && spaces[5] === spaces[8]
+    || spaces[0] === spaces[4] && spaces[4] === spaces[8]
+    || spaces[2] === spaces[4] && spaces[4] === spaces[6]
     // TODO: Check for rest of game winning cases
   )
   {
     console.log('somebody won');
     // TODO: Trigger 'game-win' event with the winning player as the event data
+  var game-win
   }
 };
 
@@ -52,6 +58,7 @@ $(document).on('click', '#board .space', function (e) {
 
 $(document).on('game-win', function (e, winner) {
   // TODO: Alert who won the game
+  alert("congratulations " + winner + " is the winner")
 });
 
 // Start the game
